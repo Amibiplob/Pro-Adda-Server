5eadcbaeba71cf3175f027102c57583b67ead131
Amibiplob <muhammadbiplob7@gmail.com>
2022-12-30 23:05:04 +0600

    post added

--- index.js
+++ index.js
@@ -4,6 +4,47 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sgq11wr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const database = client.db("Pro-adda");
+    const postCollection = database.collection("post");
+    // create a document to insert
+
+    app.post("/post", async (req, res) => {
+      const post = req.body;
+      const result = await postCollection.insertOne(post);
+      res.send(result);
+    });
+
+
+
+    app.get("/post", async (req, res) => {
+      const query= {}
+      const cursor = postCollection.find(query)
+          const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+    app.get("/personalpost", async (req, res) => {
+      const uid =req.query.email;
+      const query = {userEmail:uid};
+      const cursor = postCollection.find(query);
+      const result = await cursor.toArray();
+      console.log(result);
+      console.log(query);
+      res.send(result);
+    });
 
 
 
@@ -14,7 +55,11 @@ const port = process.env.PORT || 5000;
 
 
 
+  } finally {
+  }
+}
 
+run().catch(console.dir);
 
 
 
